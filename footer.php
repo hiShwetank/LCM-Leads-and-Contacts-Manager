6ae3f1d26e4f8fb50eeebe00fa429b7cd91d984c
Shwetank Dhar <shwetank@shwetank.co.in>
2024-12-14 15:16:21 +0530

    update footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
-    LCM v.1.3.3 Copyright &copy; <?php echo date('Y'); ?> | Powered by MICS
+    LCM v.1.3.3 Copyright &copy; <?php echo date('Y'); ?> | Powered by MICS | <a href="mailto:billdesk@mics.asia">billdesk@mics.asia</a>
 </div>
 
 </body>
